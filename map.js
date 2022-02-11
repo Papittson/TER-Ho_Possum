817554880fc26c7bd64021586eaa402842de728a
Fara Achda <farasyaninda@gmail.com>
2022-02-11 21:17:51 +0100

    Update map.js

--- map.js
+++ map.js
@@ -1,23 +1,44 @@
-function map(height, nbRows, nbColumns) {
-    d3.select("#map").append("svg").attr("width", (nbRows+1)*height).attr("height", nbRows*height);
-    let x = 50;
-    for (let row=0; row < nbRows; row++) {
-       y = 0;	     
-    for (let column=0; column < nbColumns; column++) {
-         let d = "";
-         d += "Z";
-         d3.select("svg")
-            .append("rect")
-            .attr("width", height)
-            .attr("height", height)	      
-            .attr("x", x)	      
-            .attr("y", y)	      
-            .attr("stroke", "black")
-            .attr("fill", "white")
-            .attr("id", row+" "+column);
-                   y += height;
-           }
-           x += height;
-            }
-         }
-         
+function map(height) {
+    var tile= [terre, eau, herbe, roche, foret];
+    var randomColor = tile[Math.floor(Math.random() * tile.length)];
+
+    var svg = d3.select("body").append("svg").attr("width", "500").attr("height", "500");
+    var x = 100, y = 100, height = 50;
+    svg.selectAll("rect")
+        .data(d3.range(x * y))
+        .enter()
+        .append("rect")
+        .attr("transform", translate)
+        .attr("width", height)
+        .attr("height", height)
+        .attr("stroke", "black")
+        .attr("fill", randomColor);
+
+function translate(d) {
+  return "translate(" + (d % x) * height + "," + Math.floor(d / x) * height + ")";
+}
+
+function terre() {
+   return "#a04000";
+}
+
+function eau() {
+   return "#aed6f1";
+}
+
+function herbe(){
+   return "#81c784";
+}
+
+function taniere(){
+   return "#e74c3c";
+}
+
+function roche(){
+   return  "#cacfd2";
+}
+
+function foret() {
+   return "#0e6655";
+}
+}
