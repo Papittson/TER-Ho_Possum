7324a5ac6b2c62ad347a1061fa599e1434c89198
Fara Achda <farasyaninda@gmail.com>
2022-02-17 08:58:40 +0100

    Update map.js

--- map.js
+++ map.js
@@ -1,7 +1,15 @@
 function map(height) {
-    var tile= [terre, eau, herbe, roche, foret];
-    var randomColor = tile[Math.floor(Math.random() * tile.length)];
+   var tile = {
+      soil: "#a04000",
+      water: "#aed6f1",
+      grass: "#81c784",
+      rock: "#cacfd2",
+      forest: "#0e6655"
+  
+  }
 
+  var randomColor = Object.values(tile)[Math.floor(Math.random() * Object.values(tile).length)]
+  
     var svg = d3.select("body").append("svg").attr("width", "500").attr("height", "500");
     var x = 100, y = 100, height = 50;
     svg.selectAll("rect")
@@ -16,29 +24,5 @@ function map(height) {
 
 function translate(d) {
   return "translate(" + (d % x) * height + "," + Math.floor(d / x) * height + ")";
-}
-
-function terre() {
-   return "#a04000";
-}
-
-function eau() {
-   return "#aed6f1";
-}
-
-function herbe(){
-   return "#81c784";
-}
-
-function taniere(){
-   return "#e74c3c";
-}
-
-function roche(){
-   return  "#cacfd2";
-}
-
-function foret() {
-   return "#0e6655";
 }
 }
