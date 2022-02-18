25744fba71683413a66c9e47834ebf91a32cf437
Papittson <cynthia.delgard@gmail.com>
2022-02-18 12:24:35 +0100

    ajout satisfaction besoin

--- creature.js
+++ creature.js
@@ -30,17 +30,20 @@ class Creature {
       }
     }*/
   }
-  
+
   //on a besoin des données de la case pour implémenter
   static satisfyNeeds(tileType) {
-    if(tileType == WATER){
+    if(tileType == water){
       needs.thirst == 1.75*needs.thirst;
     }
-    else if(tileType == HERB){
+    else if(tileType == grass){
       needs.hunger == 1.35*needs.hunger;
     }
-    else if (tileType == FOREST){
+    else if (tileType == forest){
       needs.hunger == 1.05*needs.hunger;
+    }
+    else if (tileType == hole){
+      needs.energy == 100;
     }
   }
   //scan de l'environnement autour de la créature dans un rayon/rectangle dont la taille dépend de la valeur de perception
