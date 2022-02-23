b7f0972ea5c6ffd676aaf5479388f8fda8259b2a
Fara Achda <farasyaninda@gmail.com>
2022-02-23 17:44:10 +0100

    map

--- map.js
+++ map.js
@@ -1,31 +1,44 @@
 function map(heightMap, heightTile) {
-  var tile = ["soil", "grass", "rock", "forest", "water"]
+  var tile = {
+      soil: 7,
+      grass: 4,
+      rock: 2,
+      forest: 3,
+      water: 1
+    }
+  var tileTotal = [];
+  var n = 0;
+  while (n < Object.keys(tile).length) {
+    for (i = 0; i <  Object.values(tile)[n]; i++)
+        tileTotal[tileTotal.length] = Object.keys(tile)[n];
+        n++; }
+
+
   var svg = d3.select("body").append("svg").attr("width", heightMap).attr("height", heightMap);
   for (let x = 0; x < 100; x++) {
-    for (let y = 0; y < 100; y++) {
-   let randomColor = Object.values(tile)[Math.floor(Math.random() * Object.values(tile).length)]
-   let randomID = tile[Math.floor(Math.random() * tile.length)]
-        svg.append("rect")
-        .attr("width", heightMap)
-        .attr("height", heightMap)
-        .attr("x", x*heightTile)
-        .attr("y", y*heightTile)
-        .attr("stroke", "black")
-        .attr("id", randomID)
-        .attr("fill", function(d) {
-          if (randomID === "water") {
-            return "#aed6f1";
-          } else if (randomID === "soil") {
-            return "#a04000";
-          } else if (randomID === "grass") {
-            return "#81c784";
-          } else if (randomID === "forest") {
-            return "#0e6655";
-          } else if (randomID === "rock") {
-            return "#cacfd2";
-          }
-        });
-
+      for (let y = 0; y < 100; y++) {
+      
+          let randomID = (tileTotal[Math.floor(Math.random() * tileTotal.length)])
+          svg.append("rect")
+          .attr("width", heightMap)
+          .attr("height", heightMap)
+          .attr("x", x*heightTile)
+          .attr("y", y*heightTile)
+          .attr("stroke", "black")
+          .attr("id", randomID)
+          .attr("fill", function(d) {
+              if (randomID === "rock") {
+              return "#cacfd2";
+              } else if (randomID === "soil") {
+              return "#a04000";
+              } else if (randomID === "grass") {
+              return "#81c784";
+              } else if (randomID === "forest") {
+              return "#0e6655";
+              } else if (randomID === "water") {
+              return "#aed6f1";
+              }
+          });
       }
     }
   }
