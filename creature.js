e7340befbcf290cd84668d9c2ccbe9bc4f30b4ec
Papittson <cynthia.delgard@gmail.com>
2022-02-25 00:49:10 +0100

    +deplacement creature , +valeurs critiques

--- creature.js
+++ creature.js
@@ -1,51 +1,43 @@
 class Creature {
   constructor(x, y, reproducibility, strenght, movespeed, perception) {
-    this.positionX = x;
-    this.positiony = y;
+    this.x = x;
+    this.y = y;
     this.reproducibility = reproducibility;
     this.strenght = strenght;
     this.movespeed = movespeed;
     this.perception = perception;
-    this.needs = { hunger: 50, thirst: 50, energy: 50 };//faire une enumération à la place (voir needs.js) et mettre les contraintes
+    this.needsLevel = { hunger: 50, thirst: 50, energy: 50 }; //faire une enumération à la place (voir needs.js) et mettre les contraintes
   }
 
   
-   //deplacement d'abord sur les x puis les y (sans obstacle) 
-  static move(x, y, tileType) {
-    //pour l'instant la créature se téléporte à l'endroit voulu
-    this.positionX = x;
-    this.positionY = y;
+  move(x, y) {
 
-    /* Voir algorithme plus court chemin, avec obstacle 
-
-    var xDistance = Math.abs(x - this.positionX);
-    var yDistance = Math.abs(y - this.positionY);
-    if (tileType == rock) {
-      if (this.positionX < x) {
-        for (let i = this.positionX; i <= xDistance; i++) {
-          xDistance=Math.abs(x-i);
-          if (this.positionY < y) {
-          }
-        }
+    const moveOnAxis = (start, end) => {
+      const step = (this.movespeed < 3) ? 1 : (this.movespeed > 3) ? 3 : 2 ;
+      const direction = (start < end) ? 1 : -1;
+  
+      while(start != end){
+          start += direction * min(Math.abs(start - end), step); 
       }
-    }*/
+    }
+    
+    moveOnAxis(this.x, x);
+    moveOnAxis(this.y, y);
+
   }
 
   //on a besoin des données de la case pour implémenter
   static satisfyNeeds(tileType) {
-    if(tileType == water){
-      needs.thirst == 1.75*needs.thirst;
-    }
-    else if(tileType == grass){
-      needs.hunger == 1.35*needs.hunger;
-    }
-    else if (tileType == forest){
-      needs.hunger == 1.05*needs.hunger;
-    }
-    else if (tileType == hole){
+    if (tileType == water) {
+      needs.thirst == 1.75 * needs.thirst;
+    } else if (tileType == grass) {
+      needs.hunger == 1.35 * needs.hunger;
+    } else if (tileType == forest) {
+      needs.hunger == 1.05 * needs.hunger;
+    } else if (tileType == hole) {
       needs.energy == 100;
     }
   }
   //scan de l'environnement autour de la créature dans un rayon/rectangle dont la taille dépend de la valeur de perception
-  static environmentAnalysis(){}
+  static environmentAnalysis() {}
 }
--- needs.js
+++ needs.js
@@ -1,5 +1,12 @@
-const needType = Object.freeze({
-    Hungry: {name: "hungry"},
-    Sleepy: {name: "sleepy"},
-    Thristy: {name: "thirsty"}
+const state = Object.freeze({
+    hungry:"hungry",
+    sleepy: "sleepy",
+    thristy: "thirsty",
+    normal:"normal"
+});
+
+const criticalLevels = Object.freeze({
+    hunger:30,
+    sleep: 20,
+    thrist: 35
 });
