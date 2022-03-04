6393c886bf3c1e02648ca519a7e8e52a318b6283
Papittson <cynthia.delgard@gmail.com>
2022-03-04 14:21:10 +0100

    test de la fonction move

--- creature.js
+++ creature.js
@@ -1,43 +1,46 @@
-
-class Creature extends Species{
+class Creature extends Species {
   constructor(x, y, reproducibility, strenght, movespeed, perception) {
-    super(reproducibility,strenght,movespeed,perception)
+    super(reproducibility, strenght, movespeed, perception);
     this.x = x;
     this.y = y;
-    this.needsLevel = { hunger: 50, thirst: 50, energy: 50 }; 
+    this.needsLevel = { hunger: 50, thirst: 50, energy: 50 };
   }
 
-  increaseNeedsLevel(need,rate){
-    if(rate<0){
-      throw RangeError;
-    }
-    this.needsLevel[need]+=rate;
+  getX() {
+    return this.x;
+  }
+  getY() {
+    return this.y;
   }
 
-  decreaseNeedsLevel(need,rate){
-    if(rate<0){
+  increaseNeedsLevel(need, rate) {
+    if (rate < 0) {
       throw RangeError;
     }
-    this.needsLevel[need]-=rate;
+    this.needsLevel[need] += rate;
   }
-  
+
+  decreaseNeedsLevel(need, rate) {
+    if (rate < 0) {
+      throw RangeError;
+    }
+    this.needsLevel[need] -= rate;
+  }
+
   move(x, y) {
-
     const moveOnAxis = (start, end) => {
-      const step = (this.movespeed < 3) ? 1 : (this.movespeed > 3) ? 3 : 2 ;
-      const direction = (start < end) ? 1 : -1;
-  
-      while(start != end){
-          start += direction * min(Math.abs(start - end), step); 
+      const step = this.movespeed < 3 ? 1 : this.movespeed > 3 ? 3 : 2;
+      const direction = start < end ? 1 : -1;
+
+      while (start != end) {
+        start += direction * Math.min(Math.abs(start - end), step);
       }
-    }
-    
+    };
+
     moveOnAxis(this.x, x);
     moveOnAxis(this.y, y);
-
   }
 
-  
   static satisfyNeeds(tileType) {
     if (tileType == water) {
       needsLevel.thirst == 1.75 * needs.thirst;
@@ -50,7 +53,5 @@ class Creature extends Species{
     }
   }
   //scan de l'environnement autour de la créature dans un rayon/rectangle dont la taille dépend de la valeur de perception
-  static environmentAnalysis() {
-      
-  }
+  static environmentAnalysis() {}
 }
