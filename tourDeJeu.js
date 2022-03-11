c815094a790f5cd56d9c01e7e427588fa3f330ae
Cyrellema <gaetanareyesn@gmail.com>
2022-03-11 15:11:48 +0100

    Update tourDeJeu.js

--- tourDeJeu.js
+++ tourDeJeu.js
@@ -8,8 +8,8 @@ function action(creature) {
         if (creature.needsLevel.thirst < seuilSoif ) { search("water",creature); return } //action recherche/search == environnementAnalysis
         if (creature.needsLevel.hunger < seuilFaim && localisationCase("grass", creature)) {eat(creature); return } //action broutage == satisfyneeds.hunger ? 
         if (creature.needsLevel.hunger < seuilFaim ) { recherche("grass", creature); return }
-        if (creature.needsLevel.sleep < seuilDodo && localisationCase("tanniere", creature)){sleep(creature); return}
-        if (creature.needsLevel.sleep < seuilDodo){search("tanniere", creature); return}
+        if (creature.needsLevel.sleep < seuilDodo && localisationCase("shed", creature)){sleep(creature); return}
+        if (creature.needsLevel.sleep < seuilDodo){search("shed", creature); return}
         if (creature.needsLevel.sleep > seuilDodo && creature.needsLevel.thrist > seuilSoif && creature.needsLevel.hunger > seuilFaim && localisationCase("tanniere", creature) /* && creature (x2) ? */){reproduce(creature); return}
         if (creature.needsLevel.sleep > seuilDodo && creature.needsLevel.thrist > seuilSoif && creature.needsLevel.hunger > seuilFaim) {search("tanniere", creature); return}
 }
