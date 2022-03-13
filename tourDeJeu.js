6e8bd98c4b65933e719d5e480dcf612d6368bb96
Cyrellema <gaetanareyesn@gmail.com>
2022-03-13 17:29:45 +0100

    Update tourDeJeu.js
    création à demi de fonction reproduce

--- tourDeJeu.js
+++ tourDeJeu.js
@@ -4,14 +4,16 @@ var seuilFaim =  [[], [], [], []] ; // idem
 var seuilDodo = [[], [], [], []] ; 
 
 function action(creature) {
-        if (creature.needsLevel.thirst < seuilSoif && localisationCase("water", creature)) {drink(creature); return }  // {satisfyNeeds(grass); return}
-        if (creature.needsLevel.thirst < seuilSoif ) { search("water",creature); return } //action recherche/search == environnementAnalysis
-        if (creature.needsLevel.hunger < seuilFaim && localisationCase("grass", creature)) {eat(creature); return } //action broutage == satisfyneeds.hunger ? 
-        if (creature.needsLevel.hunger < seuilFaim ) { recherche("grass", creature); return }
-        if (creature.needsLevel.sleep < seuilDodo && localisationCase("shed", creature)){sleep(creature); return}
-        if (creature.needsLevel.sleep < seuilDodo){search("shed", creature); return}
-        if (creature.needsLevel.sleep > seuilDodo && creature.needsLevel.thrist > seuilSoif && creature.needsLevel.hunger > seuilFaim && localisationCase("tanniere", creature) /* && creature (x2) ? */){reproduce(creature); return}
-        if (creature.needsLevel.sleep > seuilDodo && creature.needsLevel.thrist > seuilSoif && creature.needsLevel.hunger > seuilFaim) {search("tanniere", creature); return}
+        if (creature.needsLevel.thirst < seuilSoif && localisationCase("water", creature)) {satisfyNeeds(water); return}
+        if (creature.needsLevel.thirst < seuilSoif ) { search("water",creature); return } //{environnmentAnalysis(listTile); return}
+        if (creature.needsLevel.hunger < seuilFaim && localisationCase("grass", creature)) {satisfyNeeds(grass); return}
+        if (creature.needsLevel.hunger < seuilFaim ) { recherche("grass", creature); return } //{environnmentAnalysis(listTile); return}
+        if (creature.needsLevel.hunger < seuilFaim && localisationCase("forest", creature)) {satisfyNeeds(forest); return}
+        if (creature.needsLevel.hunger < seuilFaim ) { recherche("forest", creature); return } //{environnmentAnalysis(listTile); return}
+        if (creature.needsLevel.sleep < seuilDodo && localisationCase("shed", creature)){satisfyNeeds(hole); return}
+        if (creature.needsLevel.sleep < seuilDodo){search("shed", creature); return} //{environnmentAnalysis(listTile); return}
+        if (creature.needsLevel.sleep > seuilDodo && creature.needsLevel.thrist > seuilSoif && creature.needsLevel.hunger > seuilFaim && localisationCase("shed", creature) /* && creature (x2) ? */){reproduce(creature); return}
+        if (creature.needsLevel.sleep > seuilDodo && creature.needsLevel.thrist > seuilSoif && creature.needsLevel.hunger > seuilFaim) {search("shed", creature); return} //{environnmentAnalysis(listTile); return}
 }
 
 setInterval(tourDeJeu, 1000);  // 1000 millisecondes ou autre intervalle de temps
