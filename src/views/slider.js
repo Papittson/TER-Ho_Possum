9267ceb1d424e1a8cc312f8929ca11d9a66c1495
Artemis-J <291767242@qq.com>
2022-04-30 21:57:43 +0200

    Update slider.js

--- src/views/slider.js
+++ src/views/slider.js
@@ -1,8 +1,63 @@
-const sliderEl = document.querySelector("#slider-input");
+// player1
+const sliderMl = document.querySelector("#moveSpeed1");
+const selectedMl = document.querySelector("#selected_m1");
 
-const selectedEl = document.querySelector(".selected");
-
-sliderEl.addEventListener("input", () => {
-    selectedEl.innerHTML = sliderEl.value;
+sliderMl.addEventListener("input", () => {
+    selectedMl.innerHTML = sliderMl.value;
 })
+
+const sliderR1 = document.querySelector("#reproducibility1");
+const selectedR1 = document.querySelector("#selected_r1");
+
+sliderR1.addEventListener("input", () => {
+    selectedR1.innerHTML = sliderR1.value;
+})
+
+const sliderP1 = document.querySelector("#perception1");
+const selectedP1 = document.querySelector("#selected_p1");
+
+sliderP1.addEventListener("input", () => {
+    selectedP1.innerHTML = sliderP1.value;
+})
+
+const sliderS1 = document.querySelector("#strength1");
+const selectedS1 = document.querySelector("#selected_s1");
+
+sliderS1.addEventListener("input", () => {
+    selectedS1.innerHTML = sliderS1.value;
+})
+
+// player2
+const sliderM2 = document.querySelector("#moveSpeed2");
+const selectedM2 = document.querySelector("#selected_m2");
+
+sliderM2.addEventListener("input", () => {
+    selectedM2.innerHTML = sliderM2.value;
+})
+
+const sliderR2 = document.querySelector("#reproducibility2");
+const selectedR2 = document.querySelector("#selected_r2");
+
+sliderR2.addEventListener("input", () => {
+    selectedR2.innerHTML = sliderR2.value;
+})
+
+const sliderP2 = document.querySelector("#perception2");
+const selectedP2 = document.querySelector("#selected_p2");
+
+sliderP2.addEventListener("input", () => {
+    selectedP2.innerHTML = sliderP2.value;
+})
+
+const sliderS2 = document.querySelector("#strength2");
+const selectedS2 = document.querySelector("#selected_s2");
+
+sliderS2.addEventListener("input", () => {
+    selectedS2.innerHTML = sliderS2.value;
+})
+
+// player3
+
+
+// player4
 
