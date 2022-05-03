4c43ec3f8860fca2f76952400533e12cfd033690
myo <58669575+mayosine@users.noreply.github.com>
2022-05-03 11:40:07 +0200

    Add files via upload
    sliders v2

--- slider.js
+++ slider.js
@@ -1,62 +1,28 @@
-// player1
-const sliderMl = document.querySelector("#moveSpeed1");
-const selectedMl = document.querySelector("#selected_m1");
+var nbPlayers = document.getElementById('nbP').selected;
 
-sliderMl.addEventListener("input", () => {
-    selectedMl.innerHTML = sliderMl.value;
-})
+//ne marche pas avec "else"
+//affiche tous les nbPlayers si pas de "else"
 
-const sliderR1 = document.querySelector("#reproducibility1");
-const selectedR1 = document.querySelector("#selected_r1");
-
-sliderR1.addEventListener("input", () => {
-    selectedR1.innerHTML = sliderR1.value;
-})
-
-const sliderP1 = document.querySelector("#perception1");
-const selectedP1 = document.querySelector("#selected_p1");
-
-sliderP1.addEventListener("input", () => {
-    selectedP1.innerHTML = sliderP1.value;
-})
-
-const sliderS1 = document.querySelector("#strength1");
-const selectedS1 = document.querySelector("#selected_s1");
-
-sliderS1.addEventListener("input", () => {
-    selectedS1.innerHTML = sliderS1.value;
-})
-
-// player2
-const sliderM2 = document.querySelector("#moveSpeed2");
-const selectedM2 = document.querySelector("#selected_m2");
-
-sliderM2.addEventListener("input", () => {
-    selectedM2.innerHTML = sliderM2.value;
-})
-
-const sliderR2 = document.querySelector("#reproducibility2");
-const selectedR2 = document.querySelector("#selected_r2");
-
-sliderR2.addEventListener("input", () => {
-    selectedR2.innerHTML = sliderR2.value;
-})
-
-const sliderP2 = document.querySelector("#perception2");
-const selectedP2 = document.querySelector("#selected_p2");
-
-sliderP2.addEventListener("input", () => {
-    selectedP2.innerHTML = sliderP2.value;
-})
-
-const sliderS2 = document.querySelector("#strength2");
-const selectedS2 = document.querySelector("#selected_s2");
-
-sliderS2.addEventListener("input", () => {
-    selectedS2.innerHTML = sliderS2.value;
-})
-
-// player3
+//pareil avec .values;
 
 
-// player4
+if (nbPlayers == "1") {
+    console.log("one");
+}
+
+else if (nbPlayers == "2") {
+    console.log("two");
+    document.getElementById("player2").style.display = "inline-block";
+}
+
+else if (nbPlayers == "3") {
+    console.log("three");
+    document.getElementById("player3").style.display = "inline-block";
+}
+
+else if (nbPlayers == "4") {
+    console.log("four");
+    document.getElementById("player4").style.display = "inline-block";
+}
+
+
