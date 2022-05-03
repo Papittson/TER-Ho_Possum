754cc9415fd99165444bd458ac05e14f52dfc59a
Artemis-J <291767242@qq.com>
2022-05-03 15:31:16 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,33 +1,27 @@
 function player(option) {
-    if (option === "") {
-        document.getElementById('player1').style.display = "none";
-        document.getElementById('player2').style.display = "none";
-        document.getElementById('player3').style.display = "none";
-        document.getElementById('player4').style.display = "none";
-    }
     if (option === "1") {
-        document.getElementById('player1').style.display = "block";
+        document.getElementById('player1').style.display = "inline-block";
         document.getElementById('player2').style.display = "none";
         document.getElementById('player3').style.display = "none";
         document.getElementById('player4').style.display = "none";
     }
     if (option === "2") {
-        document.getElementById('player1').style.display = "none";
-        document.getElementById('player2').style.display = "block";
+        document.getElementById('player1').style.display = "inline-block";
+        document.getElementById('player2').style.display = "inline-block";
         document.getElementById('player3').style.display = "none";
         document.getElementById('player4').style.display = "none";
     }
     if (option === "3") {
-        document.getElementById('player1').style.display = "none";
-        document.getElementById('player2').style.display = "none";
-        document.getElementById('player3').style.display = "block";
+        document.getElementById('player1').style.display = "inline-block";
+        document.getElementById('player2').style.display = "inline-block";
+        document.getElementById('player3').style.display = "inline-block";
         document.getElementById('player4').style.display = "none";
     }
     if (option === "4") {
-        document.getElementById('player1').style.display = "none";
-        document.getElementById('player2').style.display = "none";
-        document.getElementById('player3').style.display = "none";
-        document.getElementById('player4').style.display = "block";
+        document.getElementById('player1').style.display = "inline-block";
+        document.getElementById('player2').style.display = "inline-block";
+        document.getElementById('player3').style.display = "inline-block";
+        document.getElementById('player4').style.display = "inline-block";
     }
 }
 document.addEventListener("DOMContentLoaded", function () {
